d34faae5df8cee7a5961d2b784f70374324e3925
Ayesha Fatima <ayesha.fatima3315l@gmail.com>
2026-06-02 20:04:06 +0530

    Implemented admin model

---
+++ employee-management-system/src/main/java/com/ayesha/employee_management_system/model/Admin.java
@@ -0,0 +1,23 @@
+package com.ayesha.employee_management_system.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Admin {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(nullable = false,unique = true)
+    private String username;
+    @Column(nullable = false)
+    private String password;
+    @Column(nullable = false,unique = true)
+    private String email;
+}
