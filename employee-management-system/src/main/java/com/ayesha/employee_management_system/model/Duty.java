42f9d859901eed555aeaff409acae383e5a72fe2
Ayesha Fatima <ayesha.fatima3315l@gmail.com>
2026-06-02 20:10:57 +0530

    Implemented duty model

---
+++ employee-management-system/src/main/java/com/ayesha/employee_management_system/model/Duty.java
@@ -0,0 +1,33 @@
+package com.ayesha.employee_management_system.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Duty {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(nullable = false)
+    private String title;
+    @Column(nullable = false)
+    private String description;
+
+    @ManyToOne
+    @JoinColumn(name = "emp_id")
+    private Employee employee;
+
+    @ManyToOne
+    @JoinColumn(name = "assignedByManager")
+    private Manager assignedByManager;
+
+    @ManyToOne
+    @JoinColumn(name = "assignedByAdmin")
+    private Admin assignedByAdmin;
+}
